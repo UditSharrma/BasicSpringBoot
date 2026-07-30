6fece94b72ade11fd9ccea9084b323c47fecb2c4
UditSharrma <atrayudit@gmail.com>
2026-07-31 01:49:26 +0530

    try to do link journal entry and user  but its not done

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
@@ -2,14 +2,15 @@ package net.engineeringdigest.journalApp.controller;
 
 
 import net.engineeringdigest.journalApp.Entity.JournalEntry;
+import net.engineeringdigest.journalApp.Entity.User;
 import net.engineeringdigest.journalApp.service.JournalEntryService;
+import net.engineeringdigest.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,15 +19,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryControllerV2 {
-
     @Autowired
     private JournalEntryService journalEntryService;
 
+    @Autowired
+    private UserService userService;
 
-
-    @GetMapping
-    public ResponseEntity<?>getAll() {
-        List<JournalEntry> all = journalEntryService.getAll();
+    @GetMapping("{userName}")
+    public ResponseEntity<?>getAllJournalEntriesOfUser(@PathVariable String userName) {
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> all = user.getJournalEntries();
 
         if (all != null && !all.isEmpty()) {
             return new ResponseEntity<>(all,HttpStatus.OK);
@@ -34,9 +36,10 @@ public class JournalEntryControllerV2 {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping
-    public ResponseEntity <JournalEntry>  createEntry(@RequestBody JournalEntry myEntry) {
+    @PostMapping("{userName}")
+    public ResponseEntity <JournalEntry>  createEntry(@RequestBody JournalEntry myEntry ,@PathVariable String userName) {
        try {
+           User user=userService.findByUserName(userName);
 
            myEntry.setDate(LocalDateTime.now());
            journalEntryService.saveEntry(myEntry);
